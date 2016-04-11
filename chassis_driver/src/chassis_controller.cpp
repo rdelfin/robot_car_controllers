9e0f06ecd7b6b8fe211073acfd8e77346478ff84
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-04-10 22:52:39 -0500

    Implemented TankDrive class

--- chassis_driver/src/chassis_controller.cpp
+++ chassis_driver/src/chassis_controller.cpp
@@ -1,5 +1,23 @@
 #include <ros/ros.h>
 
+#include <chassis_driver/TankDrive.h>
+#include <chassis_driver/TankDriveMessage.h>
+
+ros::Subscriber tankDriveSub;
+TankDrive* tankDrive;
+
+void tankDriveCallback(const chassis_driver::TankDriveMessageConstPtr& msg) {
+    
+}
+
 int main(int argc, char* argv[]) {
     ros::init(argc, argv, "chassis_drive_controller");
+    
+    ros::NodeHandle nh;
+    
+    tankDrive = new TankDrive("sparkfun_tb6612fng_controller/front", "sparkfun_tb6612fng_controller/back");
+    
+    tankDriveSub = nh.subscribe("tank_drive", 10, tankDriveCallback);
+    
+    ros::spin();
 }
